6c48d9ebb27ddf0708b17d78ad3e273b20192251
Igor Kolosov <igor.kolosov@audiocodes.com>
2021-05-03 21:24:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,49 @@
-var doc;
+'use strict';
 
-$(function () {
-  $.get('test.xml', function (data, statusText, jqXHR) {
-    $('textarea[name=sampleXML]').val(data);
-  }, 'text');
-  $('button[name=parseSample]').click(function () {
-    doc = pjXML.parse($('textarea[name=sampleXML]').val());
+let xml = `
+<?xml version="1.0"?>
+<html xmlns="http://www.w3.org/2000/svg">
+	<head>
+	<title>A swarm of motes</title>
+	<style type="text/css">
+	</style>
+	</head>
+	<body onload="update()">
+		<svg id="display" width="400" height="300">
+          <circle id="cursor1" cx="200" cy="150" r="7" fill="#0000ff" fill-opacity="0.5"/>
+          <circle id="cursor2" cx="200" cy="150" r="7" fill="#0000ff" fill-opacity="0.5"/>
+          <circle id="cursor3" cx="200" cy="150" r="7" fill="#0000ff" fill-opacity="0.5"/>
+		</svg>
+    </body>
+    </html>
+`;
 
-    $('pre[name=sampleJson]').text(JSON.stringify(doc));
-    $('pre[name=sampleText]').text(doc.text());
-    $('pre[name=sampleXML]').text(doc.xml());
-  });
-  $('input[name="xpath"]').on('change, keyup', function (e) {
-    $('pre[name=selectResult]').text(JSON.stringify(doc.select($(e.target).val())));
-  });
-});
+let doc;
+function main() {
+    console.log(xml);
+    doc = pjXML.parse(xml);
+    console.log(JSON.stringify(doc, null, 2));
+    console.log(doc.xml());
+    let e, a;
+
+    // get element
+    e = doc.select('/*/head/title');
+    console.log('title', e?.text());
+
+    // get attributes
+    a = doc.select('/*/@xmlns');
+    console.log('xmlns=', a);
+
+    a = doc.select('//body/@onload');
+    console.log('onload=', a);
+
+    // get complex element
+    e = doc.select('/html/body/svg');
+ 
+    // iterate by its sub nodes
+    for(let n of e.elements()){
+       console.log('---- svg element-----\n', n.xml());
+    }
+}
+
+main();
